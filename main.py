e028c6df6f19b8c3bd890441bc4e2b96fd49c195
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-26 18:06:58 -0400

    Create basic backend layout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather forcast for the next days")
 place = st.text_input("Place:", placeholder="New York", help="Type in the name of any place in the world")
@@ -9,8 +10,7 @@ option = st.selectbox("Option", ("Temperature", "Sky Forcast"))
 subhead_text = f"{option} for {f'the next {days} days' if days > 1 else 'today'}"
 st.subheader(subhead_text)
 
-dates = ["Mar. 4", "Mar. 6", "Mar. 7"]
-temp = [20, 25, 15]
+data = get_data(place, days, option)
 
-plot = px.line(x=dates, y=temp, labels={"x": "Dates", "y": "Temp"})
-st.plotly_chart(plot)
+# plot = px.line(x=dates, y=temp, labels={"x": "Dates", "y": "Temp"})
+# st.plotly_chart(plot)
